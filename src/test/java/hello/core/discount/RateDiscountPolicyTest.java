a58ad841351dfa2dcc81ec20a4835b3054e8bfa4
young970 <yyww9@naver.com>
2023-05-21 22:45:59 +0900

    Fix: Refactory RateDiscountPolicyTest
    RateDiscountPolicyTest에도 AppConfig를 ì 적용 강의랑 에서 배웠던서는. 없었지만 배운걸 토대로 적용

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,8 +1,10 @@
 package hello.core.discount;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
-    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    DiscountPolicy discountPolicy;
 
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        discountPolicy = appConfig.discountPolicy();
+    }
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
     void vip_o(){
